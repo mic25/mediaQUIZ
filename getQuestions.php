f50559215b54f83d86e61ffc3a19a0b5704fe059
mic25 <mirjam.mickisch@yahoo.com>
2015-07-14 16:01:41 +0200

    moved POI sorting to writeToJson - POIs now saved sorted in JSON file

--- getQuestions.php
+++ getQuestions.php
@@ -20,13 +20,6 @@ $json = file_get_contents('data/POI.json');
 //echo $json;
 $json_obj = json_decode($json, true);
 $results = $json_obj['results'];
-/* sort POIs by latitude */
-usort($results, function($a, $b){
-    if($a["geometry"]["location"]["lat"] == $b["geometry"]["location"]["lat"]){
-        return 0;
-    }
-    return $a["geometry"]["location"]["lat"] < $b["geometry"]["location"]["lat"] ? -1 : 1;
-});
 $resultLength = count($results);
 for($i = 0; $i < $resultLength; ++$i){
     $lat = $results[$i][geometry][location][lat];
@@ -367,7 +360,7 @@ for($i = 0; $i < $resultLength; ++$i){
                         echo "<br />";
                         */
                         /* if similarity of titles is higher than 85% */
-                        similar_text($pageTitle, $name, $percentage);
+                        $percentage = levenshteinPerc($pageTitle, $name);
                         if($percentage > 0.85){
                             $wikiPageId = $wikiPages_obj['query']['geosearch'][$c]['pageid'];
                             /*
@@ -418,6 +411,129 @@ function levenshteinPerc($str1, $str2) {
     } else {
         return ($len>0 ? levenshtein($str1, $str2) / $len : 1);
     }
+}
+
+function similarityOfStrings($str1, $str2) {
+    $len1 = strlen($str1);
+
+}
+
+/**
+ * A function for retrieving the Kölner Phonetik value of a string
+ *
+ * As described at http://de.wikipedia.org/wiki/Kölner_Phonetik
+ * Based on Hans Joachim Postel: Die Kölner Phonetik.
+ * Ein Verfahren zur Identifizierung von Personennamen auf der
+ * Grundlage der Gestaltanalyse.
+ * in: IBM-Nachrichten, 19. Jahrgang, 1969, S. 925-931
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @package phonetics
+ * @version 1.0
+ * @link http://www.einfachmarke.de
+ * @license GPL 3.0 <http://www.gnu.org/licenses/>
+ * @copyright  2008 by einfachmarke.de
+ * @author Nicolas Zimmer <nicolas dot zimmer at einfachmarke.de>
+ */
+
+function cologne_phon($word) {
+    /**
+     * @param  string  $word string to be analyzed
+     * @return string  $value represents the Kölner Phonetik value
+     * @access public
+     */
+
+    // prepare for processing
+    $word = strtolower($word);
+    $substitution = array(
+        "ä"=>"a",
+        "ö"=>"o",
+        "ü"=>"u",
+        "ß"=>"ss",
+        "ph"=>"f"
+    );
+
+    foreach ($substitution as $letter => $substitution) {
+        $word = str_replace($letter,$substitution,$word);
+    }
+
+    $len = strlen($word);
+
+    // Rule for exeptions
+    $exceptionsLeading = array(
+        4 => array("ca","ch","ck","cl","co","cq","cu","cx"),
+        8 => array("dc","ds","dz","tc","ts","tz")
+    );
+
+    $exceptionsFollowing = array("sc","zc","cx","kx","qx");
+
+    //Table for coding
+    $codingTable = array(
+        0  => array("a", "e", "i", "j", "o", "u", "y"),
+        1  => array("b", "p"),
+        2  => array("d", "t"),
+        3  => array("f", "v", "w"),
+        4  => array("c", "g", "k", "q"),
+        48 => array("x"),
+        5  => array("l"),
+        6  => array("m", "n"),
+        7  => array("r"),
+        8  => array("c", "s", "z"),
+    );
+
+    for ($i=0; $i<$len; $i++) {
+        $value[$i] = "";
+
+        //Exceptions
+        if ($i == 0 && $word[$i].$word[$i+1] == "cr") {
+            $value[$i] = 4;
+        }
+
+        foreach ($exceptionsLeading as $code => $letters) {
+            if (in_array($word[$i].$word[$i+1], $letters)) {
+                $value[$i] = $code;
+            }
+        }
+
+        if ($i != 0 && (in_array($word[$i-1].$word[$i], $exceptionsFollowing))) {
+            $value[$i] = 8;
+        }
+
+        // normal encoding
+        if ($value[$i] == "") {
+            foreach ($codingTable as $code => $letters) {
+                if (in_array($word[$i], $letters)) {
+                    $value[$i] = $code;
+                }
+            }
+        }
+    }
+
+    // delete double values
+    $len = count($value);
+
+    for ($i=1; $i<$len; $i++) {
+        if ($value[$i] == $value[$i-1]) {
+            $value[$i] = "";
+        }
+    }
+
+    // delete vocals
+    for ($i=1; $i>$len; $i++) {
+        // omitting first characer code and h
+        if ($value[$i] == 0) {
+            $value[$i] = "";
+        }
+    }
+
+    $value = array_filter($value);
+    $value = implode("", $value);
+
+    return $value;
 }
 
 ?>
--- writeToJson.php
+++ writeToJson.php
@@ -3,7 +3,17 @@
 $json = file_get_contents('https://maps.googleapis.com/maps/api/place/radarsearch/json?location=48.1549108,11.5418358&radius=500000&types=(airport|amusement_park|aquarium|art_gallery|cemetery|city_hall|embassy|hospital|library|movie_theater|museum|night_club|park|place_of_worship|police|shopping_mall|stadium|train_station|university|zoo)&key=AIzaSyAhFHDr_1SlAdzp2G0OfM7p9kw-QI9IUCs');
 
 $jsonfile = fopen("data/POI.json", "w") or die("Unable to open file!");
-fwrite($jsonfile, $json);
+$json_obj = json_decode($json, true);
+$results = $json_obj['results'];
+/* sort POIs by latitude */
+usort($results, function($a, $b){
+    if($a["geometry"]["location"]["lat"] == $b["geometry"]["location"]["lat"]){
+        return 0;
+    }
+    return $a["geometry"]["location"]["lat"] < $b["geometry"]["location"]["lat"] ? -1 : 1;
+});
+$jsonNew = json_encode($results);
+fwrite($jsonfile, $jsonNew);
 fclose($jsonfile);
 
 echo "wrote JSON To File <br />";
