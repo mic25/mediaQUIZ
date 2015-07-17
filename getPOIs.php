2ac263371961d2b6d9fc9a1d2e1c05a4d675615d
mic25 <mirjam.mickisch@yahoo.com>
2015-07-17 12:57:15 +0200

    add crowdsourcing filter

--- getPOIs.php
+++ getPOIs.php
@@ -36,9 +36,94 @@ for($i = 0; $i < $resultLength; ++$i){
     $types = $details_obj[result][types];
 
     if ($result = mysqli_query($link, $query)) {
+        $videos = [];
+        if(mysqli_num_rows($result) > 0) {
+            $rows = [];
+            $videoFrames = [];
+
+            while ($row = mysqli_fetch_array($result)) {
+                array_push($rows, $row);
+            }
+            /* free result set */
+            mysqli_free_result($result);
+
+            $rowsLength = count($rows);
+            for ($r = 0; $r < $rowsLength; ++$r) {
+                $id = $rows[$r]['VideoId'];
+                $frame = $rows[$r]['FovNum'];
+                $frames = [];
+                $contained = false;
+                $index = 0;
+                $videoFramesLength = count($videoFrames);
+                for ($v = 0; $v < $videoFramesLength; ++$v) {
+                    if ($videoFrames[$v]['id'] == $id) {
+                        $contained = true;
+                        $index = $v;
+                    }
+                }
+                if (!$contained) {
+                    /* push new object with id and frame to array */
+                    $frames['id'] = $id;
+                    $frames['frames'] = [];
+                    array_push($frames['frames'], $frame);
+                    array_push($videoFrames, $frames);
+                } else {
+                    /* push frame to object with id */
+                    array_push($videoFrames[$index]['frames'], $frame);
+                }
+            }
+            $videoFramesLength = count($videoFrames);
+            for ($v = 0; $v < $videoFramesLength; ++$v) {
+                if (count($videoFrames[$v]['frames']) > 2) {
+                    sort($videoFrames[$v]['frames']);
+                    $following = 0;
+                    $usableFrames = [];
+                    $usableFramesArray = [];
+                    $maxCount = count($videoFrames[$v]['frames']) - 1;
+                    for ($f = 0; $f < $maxCount; ++$f) {
+                        if ($videoFrames[$v]['frames'][$f + 1] - $videoFrames[$v]['frames'][$f] == 1) {
+                            $following++;
+                            if (!in_array($videoFrames[$v]['frames'][$f], $usableFrames)) {
+                                array_push($usableFrames, $videoFrames[$v]['frames'][$f]);
+                            }
+                            if (!in_array($videoFrames[$v]['frames'][$f + 1], $usableFrames)) {
+                                array_push($usableFrames, $videoFrames[$v]['frames'][$f + 1]);
+                            }
+                        } else {
+                            $usableFramesLength = count($usableFrames);
+                            if ($usableFramesLength < 3) {
+                                $following = 0;
+                                $usableFrames = [];
+                            } else {
+                                array_push($usableFramesArray, $usableFrames);
+                                $usableFrames = [];
+                            }
+                        }
+                        if ($f == $maxCount - 1) {
+                            $usableFramesLength = count($usableFrames);
+                            if ($usableFramesLength < 3) {
+                                $following = 0;
+                                $usableFrames = [];
+                            } else {
+                                array_push($usableFramesArray, $usableFrames);
+                                $usableFrames = [];
+                            }
+                        }
+                    }
+                    $usableFramesArrayLength = count($usableFramesArray);
+                    if ($usableFramesArrayLength > 0) {
+                        for ($ufa = 0; $ufa < $usableFramesArrayLength; ++$ufa) {
+                            $video['id'] = $videoFrames[$v]['id'];
+                            $video['usableFrames'] = $usableFramesArray[$ufa];
+                            array_push($videos, $video);
+                        }
+                    }
+                }
+            }
+        }
         $value = array(
             "name" => $name,
-            "videos" => mysqli_num_rows($result),
+            "videos" => count($videos),
             "lat" => $lat,
             "lng" => $lng
         );
